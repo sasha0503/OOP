42b2d425b3cfcefe4c2525ed2eeb65260305265d
Oleksandr Lyuklyan <74149222+sasha0503@users.noreply.github.com>
2021-10-11 23:11:19 +0300

    Add first triangle exercise

---
+++ homeworks/homework4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace triangle
+{
+    static class Program
+    {
+        public class Triangle
+        {
+            protected double side1, side2, side3;
+            public Triangle(double a, double b, double c)
+            {
+                side1 = a;
+                side2 = b;
+                side3 = c;
+            }
+            public double GetParam()
+            {
+                return side1 + side2 + side3;
+            }
+            public List<double> GetAngles()
+            {
+                double a = Math.Acos((side2 * side2 + side3 * side3 - side1 * side1) / (2 * side2 * side3));
+                double b = Math.Acos((side1 * side1 + side3 * side3 - side2 * side2) / (2 * side1 * side3));
+                double c = Math.Acos((side1 * side1 + side2 * side2 - side3 * side3) / (2 * side1 * side2));
+                List<double> list = new List<double> { a, b, c };
+                return list;
+            }
+            public void SetSides(double a, double b, double c)
+            {
+                side1 = a;
+                side2 = b;
+                side3 = c;
+            }
+        }
+
+        public class RegularTriangle : Triangle
+        {
+            public RegularTriangle(double a) : base(a, a, a)
+            {
+            }
+            private double Area;
+            public double GetArea()
+            {
+                double A = Math.Sqrt(3) * 0.25 * side1 * side1;
+                this.Area = A;
+                return A;
+            }
+        }
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
